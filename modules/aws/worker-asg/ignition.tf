35fc5e8bf1e2752101e4b2ed950f24b9908ec35d
Bryan Shelton <bryan@rover.com>
2018-01-15 15:48:50 -0800

    AWS Domain search path

--- modules/aws/worker-asg/ignition.tf
+++ modules/aws/worker-asg/ignition.tf
@@ -4,6 +4,7 @@ data "ignition_config" "main" {
     "${var.ign_installer_runtime_mappings_id}",
     "${var.ign_max_user_watches_id}",
     "${var.ign_s3_puller_id}",
+    "${data.ignition_file.resolved_conf_dropin.id}",
   ]
 
   systemd = [
@@ -12,4 +13,21 @@ data "ignition_config" "main" {
     "${var.ign_kubelet_service_id}",
     "${var.ign_locksmithd_service_id}",
   ]
+}
+
+data "aws_region" "current" {
+  current = true
+}
+
+data "ignition_file" "resolved_conf_dropin" {
+  filesystem = "root"
+  path       = "/etc/systemd/resolved.conf.d/10-resolved-conf.conf"
+  mode       = 0644
+
+  content {
+    content = <<EOF
+[Resolve]
+Domains=${data.aws_region.current.name == "us-east-1" ? "ec2.internal" : "${data.aws_region.current.name}.compute.internal"}
+EOF
+  }
 }
